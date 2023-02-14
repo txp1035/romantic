5faa2b2096940f327e58f1b582e34871c7c139ff
DaoYuan <txp1035@gmail.com>
2023-02-14 22:48:07 +0800

    chore: all

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,10 +4,23 @@ import { useEffect, useState } from 'react';
 import Heart from './a.js';
 // import { Modal, Spin } from 'antd';
 import { Modal, Toast } from 'antd-mobile';
+import { useLocation } from '@umijs/max';
+import second from './weixin.png';
+import hash from 'object-hash';
+import moment from 'dayjs';
+
+import qs from 'query-string';
+
 let heart;
 const HomePage: React.FC = () => {
   const [first, setfirst] = useState(true);
+  const location = useLocation();
 
+  const obj = qs.parse(location.search);
+  const strHour = moment().format('YYYY-MM-DD HH');
+  const shortHashHour = hash(strHour).slice(0, 7).toLocaleUpperCase();
+  const isHide = obj?.d?.toLocaleUpperCase() === shortHashHour;
+  console.log(123, obj);
   useEffect(() => {
     Toast.show({
       icon: 'loading',
@@ -38,12 +51,20 @@ const HomePage: React.FC = () => {
     <PageContainer ghost>
       {/* <Spin tip="加载中" spinning={first}> */}
       <div className={styles.container}>
+        <h1>
+          {obj.a} {obj.b} {obj.c}
+        </h1>
         <canvas id="heart"></canvas>
         <audio loop>
           <source src="/heart.mp3" type="audio/mpeg" />
         </audio>
+        {!isHide && (
+          <>
+            <img src={second} alt="" width="100%" />
+            <span>关注公众号回复【heart】去掉二维码</span>
+          </>
+        )}
       </div>
-      {/* </Spin> */}
     </PageContainer>
   );
 };
