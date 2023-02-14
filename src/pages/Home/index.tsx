03eb55a24c82c92bd965e1674fa6c83c53fb0ab2
DaoYuan <txp1035@gmail.com>
2023-02-14 21:57:27 +0800

    chore: load

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,41 @@
 import { PageContainer } from '@ant-design/pro-components';
 import styles from './index.less';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Heart from './a.js';
-import { Modal } from 'antd';
-
+import { Modal, Spin } from 'antd';
+let heart;
 const HomePage: React.FC = () => {
+  const [first, setfirst] = useState(true);
+
   useEffect(() => {
-    const heart = new Heart();
-    Modal.confirm({
-      title: '是否播放心跳声',
-      onCancel: () => {
-        heart.draw();
-      },
-      onOk: () => {
-        heart.draw();
-        const a = document.querySelector('audio');
-        a.play();
-      },
-    });
+    heart = new Heart();
+    setfirst(false);
   }, []);
+  useEffect(() => {
+    if (!first) {
+      Modal.confirm({
+        title: '是否播放心跳声',
+        onCancel: () => {
+          heart.draw();
+        },
+        onOk: () => {
+          heart.draw();
+          const a = document.querySelector('audio');
+          a.play();
+        },
+      });
+    }
+  }, [first]);
   return (
     <PageContainer ghost>
-      <div className={styles.container}>
-        <canvas id="heart"></canvas>
-        <audio loop>
-          <source src="/heart.mp3" type="audio/mpeg" />
-        </audio>
-      </div>
+      <Spin tip="加载中" spinning={first}>
+        <div className={styles.container}>
+          <canvas id="heart"></canvas>
+          <audio loop>
+            <source src="/heart.mp3" type="audio/mpeg" />
+          </audio>
+        </div>
+      </Spin>
     </PageContainer>
   );
 };
