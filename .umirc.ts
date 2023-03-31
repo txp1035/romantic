bd173e4b0d23cc65628488cce4e0bfa221f0b142
DaoYuan <txp1035@gmail.com>
2023-03-31 21:17:27 +0800

    chore: statistics

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   npmClient: 'pnpm',
+  headScripts: [{ src: '/baidu.js', defer: true }],
   history: {
     type: 'browser',
   },
---
+++ public/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e16765175cf50bd49495092a680b871b";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
